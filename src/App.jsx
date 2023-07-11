32511c9acb119ef33fad5d7d33cd22e24c11dbfc
choiruladamm <choiruladamm@gmail.com>
2023-07-11 15:45:47 +0700

    Drag And Drop Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 
+import DragAndDrop from "./components/DragAndDrop/DnD"
 import InfiniteScrolling from "./components/InfiniteScrolling/InfiniteScrolling"
 import Pagination from "./components/Pagination/Pagination"
 
 const App = () => {
   return (
-    <div className="font-[Inter]">
-      <InfiniteScrolling />
+    <div className="font-[Inter] h-screen grid place-items-center">
+      <DragAndDrop />
     </div>
   )
 }
